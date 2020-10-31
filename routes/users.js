94b1db6f562a4c5619a687aa1ba490b479476a0e
gideon-chung <63383138+gideon-chung@users.noreply.github.com>
2020-10-30 23:05:36 -0400

    dummy database for users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,73 @@
 var express = require('express');
 var router = express.Router();
 
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'google',
+      password: 'apple',
+      groups: [
+
+        {
+          id: '123',
+          name: 'google',
+          description: 'we google things',
+          likes: 0,
+          members: 0,
+          created: new Date(),
+          deleted: false,
+        },
+        {
+          id: '124',
+          name: 'yahoo',
+          description: 'we yahoo things',
+          likes: 0,
+          members: 0,
+          created: new Date(),
+          deleted: false
+        }
+
+      ],
+      created: new Date(),
+      deleted: false,
+    },
+    {
+      id: '123',
+      name: 'Yahoo',
+      password: 'yes',
+      groups: [
+
+        {
+          id: '123',
+          name: 'google',
+          description: 'we google things',
+          likes: 0,
+          members: 0,
+          created: new Date(),
+          deleted: false,
+        },
+        {
+          id: '124',
+          name: 'yahoo',
+          description: 'we yahoo things',
+          likes: 0,
+          members: 0,
+          created: new Date(),
+          deleted: false
+        }
+
+      ],
+      created: new Date(),
+      deleted: false,
+    },
+  ]
+}
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
