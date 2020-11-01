6a3ed22b65663da83607d207f8b1eac1dffedf2c
Tate Fowble <meyus@Tates-MacBook-Pro.local>
2020-10-31 22:10:58 -0500

    fixed changes

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var profileRouter = require('./routes/posts');
-var postRouter = require('./routes/posts');
-var groupsRouter = require('./routes/groups');
+var profileRouter = require('./routes/profile');
+// var postsRouter = require('./routes/posts');
+// var groupsRouter = require('./routes/groups');
 
 
 /////////////////////////////////////////////////
@@ -38,8 +38,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/profile', profileRouter);
-app.use('/posts', postsRouter);
-app.use('/groups', groupsRouter);
+// app.use('/posts', postsRouter);
+// app.use('/groups', groupsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
