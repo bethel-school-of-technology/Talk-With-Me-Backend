9a37025e29df4a9ea00e720c60c29d1f1d6ce640
TateFowble <tatefowble@gmail.com>
2020-11-15 23:30:30 -0600

    login is basically done

--- routes/signin.js
+++ routes/signin.js
@@ -3,12 +3,16 @@ const { FailedDependency } = require('http-errors');
 var router = express.Router();
 let User = require('../models/User');
 var authService = require('../services/auth');
-const bcrypt = require('bcrypt-nodejs');
 
 
 router.get('/', async (req, res, next) => {
     try {
-        const users = await User.find({});
+        const users = await User.find({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password
+        });
 
         res.status(200).json({
             data: { users }
@@ -24,26 +28,47 @@ router.get('/', async (req, res, next) => {
 
 
 router.post('/', async (req, res, next) => {
-    try {
-        const newUser = await User.find(req.body);
-        console.log(newUser);
-        res.status(201).json({
-            data: { user: newUser }
+    // try {
+    //     const newUser = await User.findOne({
+    //         firstname: req.body.firstname,
+    //         email: req.body.email
+    //     });
+    //     console.log(newUser);
+    //     res.status(201).json({
+    //         data: { user: newUser }
+    //     });
+    // } catch {
+    //     console.log('error is called')
+    //     console.log(err);
+    //     res.status(400).json({
+    //         status: 'failed to find user',
+    //         message: err
+    //     });
+    // }
+        const newUser = await User.findOne({
+            email: req.body.email
+        }).then(user => {
+            console.log(user)
+            if(!user){
+                console.log('error is called')
+                console.log(err);
+                res.status(400).json({
+                status: 'failed to find user',
+                message: err
+                });
+            } else {
+                if (user) {
+                    let passwordMatch = authService.comparePasswords(req.body.password, user.password)
+                    if (passwordMatch) {
+                        let token = authService.signUser(User);
+                        res.cookie('jwt', token);
+                        res.send('Login successful');
+                    }
+                } else {
+                    res.send('wrong password')
+                }
+            }
         });
-    } catch (err) {
-        res.status(400).json({
-            status: 'failed to find user',
-            message: err
-        });
-    }
-        if (user) {
-            let token = authService.signUser(user); // <--- Uses the authService to create jwt token
-            res.cookie('jwt', token); // <--- Adds token to response as a cookie
-            res.send('Login successful');
-        } else {
-            console.log('Wrong password');
-            res.redirect('login')
-        }
     });
 
 module.exports = router;
